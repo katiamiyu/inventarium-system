6eb42432b6d23833dbd5866deb9f59dfef619082
katiamiyu <katiamiyu@gmail.com>
2020-03-09 13:56:02 +0100

    feat(remove employee by id):
    enter valid endpoint address
    check employees available in database
    remove  available employees with id
    
    [Finishes #171513837]

--- server/tests/emp.js
+++ server/tests/emp.js
@@ -142,4 +142,144 @@ describe('Test on employee endpoints', () => {
     });
     // end of get all employees end-point
   });
+  describe('Update employees endpoint', () => {
+    // create employee
+    it('should update employee record', (done) => {
+      request(app)
+        .put('/api/v1/employees/1')
+        .send({
+          empName: 'Adebayo tiamiyu',
+          mobile: '8177221305',
+          deptId: 6,
+        })
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(201)
+        .end((err, res) => {
+          expect(res.body.message).to.equal('employee updated successfully');
+          done();
+        });
+    });
+    // check for emp name
+    it('should check for employee name', (done) => {
+      request(app)
+        .put('/api/v1/employees/1')
+        .send({
+          mobile: '8177221305',
+          deptId: 6,
+        })
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(201)
+        .end((err, res) => {
+          expect(res.body.errors[0].msg).to.equal('Employee name is required');
+          done();
+        });
+    });
+    // check for emp mobile
+    it('should check for employee mobile', (done) => {
+      request(app)
+        .put('/api/v1/employees/1')
+        .send({
+          empName: 'kazeem tiamiyu',
+          deptId: 4,
+        })
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(201)
+        .end((err, res) => {
+          expect(res.body.errors[0].msg).to.equal('Mobile is required');
+          done();
+        });
+    });
+    // check if mobile is a number
+    it('should check for employee mobile', (done) => {
+      request(app)
+        .put('/api/v1/employees/1')
+        .send({
+          empName: 'kazeem tiamiyu',
+          mobile: '8177errf',
+          deptId: 4,
+        })
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(201)
+        .end((err, res) => {
+          expect(res.body.errors[0].msg).to.equal('Mobile can not be alphanumeric');
+          done();
+        });
+    });
+    // check for department id
+    it('should check for employees department', (done) => {
+      request(app)
+        .put('/api/v1/employees/1')
+        .send({
+          empName: 'kazeem tiamiyu',
+          mobile: '8177221305',
+        })
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(201)
+        .end((err, res) => {
+          expect(res.body.errors[0].msg).to.equal('Department id is required');
+          done();
+        });
+    });
+    // check if department id is a number
+    it('should check for employee mobile', (done) => {
+      request(app)
+        .put('/api/v1/employees/1')
+        .send({
+          empName: 'kazeem tiamiyu',
+          mobile: '8177221305',
+          deptId: 'is dept',
+        })
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(201)
+        .end((err, res) => {
+          expect(res.body.errors[0].msg).to.equal('Mobile should be a number');
+          done();
+        });
+    });
+    // end of update employee end-point.
+  });
+  describe('remove employee(s) endpoint', () => {
+    it('should check for valid employee id', (done) => {
+      request(app)
+        .delete('/api/v1/employees/one')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(201)
+        .end((err, res) => {
+          expect(res.body.message).to.equal('invalid id');
+          done();
+        });
+    });
+    it('should remove employee by id', (done) => {
+      request(app)
+        .delete('/api/v1/employees/1')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(201)
+        .end((err, res) => {
+          expect(res.body.message).to.equal('employee removed successfully');
+          done();
+        });
+    });
+    // end of get all employees end-point
+  });
+  describe('Errors from wrong endpoints', () => {
+    it('should throw error for invalid endpoint', (done) => {
+      request(app)
+        .get('/api/v1/employee')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(201)
+        .end((err, res) => {
+          expect(res.body.status).to.equal(404);
+          done();
+        });
+    });
+  });
 });
