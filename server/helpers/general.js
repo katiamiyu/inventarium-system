033c1e11e489a8eba9d7bdb0ca569af1ad664243
unknown <jamesugbanu@gmail.com>
2020-03-15 11:11:59 +0100

    configure travis-ci, coveralls and codeclimate

--- server/helpers/general.js
+++ server/helpers/general.js
@@ -1,7 +1,7 @@
-const checkId = id => {
-    const newId = parseInt(id, 10);
-    return isNaN(newId) ? 0 : newId;
-    }
+const checkId = (id) => {
+  const newId = parseInt(id, 10);
+  return isNaN(newId) ? 0 : newId;
+};
 
 
 export default checkId;
